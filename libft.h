79293005cfd4dd349ff0c3e24a6dc19971cf72f0
Phufa Somchanakit <phufsomc@z2t17c4.42bangkok.com>
2026-08-24 18:00:53 +0700

    init

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phufsomc <phufsomc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/24 16:38:14 by phufsomc          #+#    #+#             */
+/*   Updated: 2026/08/24 17:29:19 by phufsomc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+
+#endif
